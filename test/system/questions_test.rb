030cfd098c6c88107dd84c1655db8d399da38f6b
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-03 13:39:08 +0900

    created schema and tests

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -3,6 +3,22 @@ require "application_system_test_case"
 class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
     visit ask_url
-    assert_selector "p", text: "Ask your coach anything"
+    assert_selector "h1", text: "STUPID COACH"
+  end
+  # test "Coach answers a question" do
+  #   visit answer_url
+  #   fill_in "question", with: "What time is it?"
+  #   click_on "Ask"
+
+  #   assert_text "Silly question, get dressed and go to work!"
+  # end
+
+    test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
   end
 end
+
